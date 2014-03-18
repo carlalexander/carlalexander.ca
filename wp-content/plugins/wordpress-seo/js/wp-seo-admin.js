7a66f99e8cc063140605ef0361a2859e56a17357
Carl Alexander <carlalexander@gmail.com>
2014-03-18 15:53:52 -0400

    Updated "WordPress SEO"

--- wp-content/plugins/wordpress-seo/js/wp-seo-admin.js
+++ wp-content/plugins/wordpress-seo/js/wp-seo-admin.js
@@ -1,26 +1,49 @@
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
+	
+	/* Fix banner images overlapping help texts */
+	jQuery('.screen-meta-toggle a').click( function() {
+		jQuery("#sidebar-container").toggle();
+	});
+
+	// events
 	jQuery("#enablexmlsitemap").change(function() {
-		if (jQuery("#enablexmlsitemap").is(':checked')) {
-			jQuery("#sitemapinfo").css("display","block");
-		} else {
-			jQuery("#sitemapinfo").css("display","none");
-		}
+		jQuery("#sitemapinfo").toggle(jQuery(this).is(':checked'));
 	}).change();
+
 	jQuery("#cleanpermalinks").change(function() {
-		if (jQuery("#cleanpermalinks").is(':checked')) {
-			jQuery("#cleanpermalinksdiv").css("display","block");
-		} else {
-			jQuery("#cleanpermalinksdiv").css("display","none");
-		}
-	}).change();		
+		jQuery("#cleanpermalinksdiv").toggle(jQuery(this).is(':checked'));
+	}).change();
+
+	jQuery('#wpseo-tabs').find('a').click(function() {
+		jQuery('#wpseo-tabs').find('a').removeClass('nav-tab-active');
+		jQuery('.wpseotab').removeClass('active');
+
+		var id = jQuery(this).attr('id').replace('-tab','');
+		jQuery('#' + id).addClass('active');
+		jQuery(this).addClass('nav-tab-active');
+	});
+
+	// init
+	var active_tab = window.location.hash.replace('#top#','');
+
+	// default to first tab
+	if ( active_tab == '' || active_tab == '#_=_') {
+		active_tab = jQuery('.wpseotab').attr('id');
+	}
+
+	jQuery('#' + active_tab).addClass('active');
+	jQuery('#' + active_tab + '-tab').addClass('nav-tab-active');
+
 });
 
+
+// global functions
 function setWPOption( option, newval, hide, nonce ) {
-	jQuery.post(ajaxurl, { 
-			action: 'wpseo_set_option', 
+	jQuery.post(ajaxurl, {
+			action: 'wpseo_set_option',
 			option: option,
 			newval: newval,
-			_wpnonce: nonce 
+			_wpnonce: nonce
 		}, function(data) {
 			if (data)
 				jQuery('#'+hide).hide();
@@ -40,19 +63,15 @@ function wpseo_killBlockingFiles( nonce ) {
 	});
 }
 
-jQuery(document).ready(function(){	
-	var active_tab = window.location.hash.replace('#top#','');
-	if ( active_tab == '' )
-		active_tab = jQuery('.wpseotab').attr('id');
-	jQuery('#'+active_tab).addClass('active');
-	jQuery('#'+active_tab+'-tab').addClass('nav-tab-active');
-	
-	jQuery('#wpseo-tabs a').click(function() {
-		jQuery('#wpseo-tabs a').removeClass('nav-tab-active');
-		jQuery('.wpseotab').removeClass('active');
-	
-		var id = jQuery(this).attr('id').replace('-tab','');
-		jQuery('#'+id).addClass('active');
-		jQuery(this).addClass('nav-tab-active');
+/*jQuery(document).ready(function(){
+	// Collapsible debug information on the settings pages
+	jQuery('#wpseo-debug-info').accordion({
+		active: false,
+		collapsible: true,
+		icons: {
+			header: 'ui-icon-circle-triangle-e',
+			activeHeader: 'ui-icon-circle-triangle-s'
+		},
+		heightStyle: 'content'
 	});
-});
+});*/
