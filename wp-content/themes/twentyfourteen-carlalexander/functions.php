d1c66614bea27c8260f2fbc0ddc9ac2b82d17948
Carl Alexander <carlalexander@gmail.com>
2015-03-01 10:00:16 -0500

    Added support for Speaker Deck oEmbed

--- wp-content/themes/twentyfourteen-carlalexander/functions.php
+++ wp-content/themes/twentyfourteen-carlalexander/functions.php
@@ -5,6 +5,33 @@
  */
 
 /**
+ * Add custom oEmbed providers.
+ */
+function twentyfourteen_add_oembed_providers() {
+    wp_oembed_add_provider('#https?://speakerdeck.com/*/*#i', 'https://speakerdeck.com/oembed.json', true);
+}
+add_action('init', 'twentyfourteen_add_oembed_providers');
+
+/**
+ * Add a wrapper div to all oembeds with styling classes.
+ *
+ * @param string $html
+ * @param object $data
+ *
+ * @return string
+ */
+function twentyfourteen_add_oembed_div($html, $data) {
+    $class = 'oembed';
+
+    if (!empty($data->provider_name)) {
+        $class .= ' oembed-'.sanitize_title($data->provider_name);
+    }
+
+    return '<div class="'.$class.'">'.$html.'</div>';
+}
+add_filter('oembed_dataparse', 'twentyfourteen_add_oembed_div', 10, 2);
+
+/**
  * Print HTML with meta information for the current post-date/time and author.
  */
 function twentyfourteen_posted_on() {
