3747d51c5729735caecb539a1689dd0c1c0c3f07
Carl Alexander <carlalexander@gmail.com>
2017-07-17 08:14:47 -0400

    Configure dev environment to use mailhog

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,11 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+define('WPMS_ON', true);
+define('WPMS_MAILER', 'smtp');
+define('WPMS_SET_RETURN_PATH', 'false');
+define('WPMS_SSL', '');
+define('WPMS_SMTP_AUTH', false);
+define('WPMS_SMTP_HOST', 'localhost');
+define('WPMS_SMTP_PORT', 1025);
