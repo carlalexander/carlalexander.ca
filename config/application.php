b2bc7cfbf59364e8b7cabeba72240c96b384aad8
Carl Alexander <carlalexander@gmail.com>
2017-09-02 15:56:46 -0600

    Rollback multisite site config so that I can turn it on

--- config/application.php
+++ config/application.php
@@ -42,12 +42,12 @@ define('WP_SITEURL', env('WP_SITEURL'));
  * Multisite
  */
 define('WP_ALLOW_MULTISITE', true);
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', parse_url(WP_HOME,  PHP_URL_HOST));
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+//define('MULTISITE', true);
+//define('SUBDOMAIN_INSTALL', true);
+//define('DOMAIN_CURRENT_SITE', parse_url(WP_HOME,  PHP_URL_HOST));
+//define('PATH_CURRENT_SITE', '/');
+//define('SITE_ID_CURRENT_SITE', 1);
+//define('BLOG_ID_CURRENT_SITE', 1);
 
 /**
  * Custom Content Directory
