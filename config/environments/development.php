c7f747da5e1809a309997afb6e55024dccd49f18
Carl Alexander <carlalexander@gmail.com>
2019-05-21 18:22:00 -0400

    Update to the latest Bedrock release

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,13 +1,25 @@
 <?php
-/** Development */
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
-define('SCRIPT_DEBUG', true);
+/**
+ * Configuration overrides for WP_ENV === 'development'
+ */
 
-define('WPMS_ON', true);
-define('WPMS_MAILER', 'smtp');
-define('WPMS_SET_RETURN_PATH', 'false');
-define('WPMS_SSL', '');
-define('WPMS_SMTP_AUTH', false);
-define('WPMS_SMTP_HOST', 'localhost');
-define('WPMS_SMTP_PORT', 1025);
+use Roots\WPConfig\Config;
+
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SCRIPT_DEBUG', true);
+
+ini_set('display_errors', 1);
+
+// Enable plugin and theme updates and installation from the admin
+Config::define('DISALLOW_FILE_MODS', false);
+
+Config::define('WPMS_ON', true);
+Config::define('WPMS_MAILER', 'smtp');
+Config::define('WPMS_SET_RETURN_PATH', 'false');
+Config::define('WPMS_SSL', '');
+Config::define('WPMS_SMTP_AUTH', false);
+Config::define('WPMS_SMTP_HOST', 'localhost');
+Config::define('WPMS_SMTP_PORT', 1025);
